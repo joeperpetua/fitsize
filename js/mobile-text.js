36779a9e7c250fd588ba7e50214c9dc89b66788a
joeperpetua <qwerty.perpetua@gmail.com>
2020-09-21 16:12:37 +0200

    Added the custom text for each mobile slide

--- js/mobile-text.js
+++ js/mobile-text.js
@@ -1,30 +1,37 @@
 let slidesContainer;
 let txtTag;
+let descTag;
 window.onload = () => {
     slidesContainer = document.getElementsByClassName('slides')[0];
     txtTag = document.getElementById('slide-title');
+    descTag = document.getElementById('slide-desc');
 
     slidesContainer.onscroll = () => {
         //console.log(slidesContainer.scrollLeft);
         switch (slidesContainer.scrollLeft) {
             case 0:
-                txtTag.innerHTML = 'Text 1';
+                txtTag.innerHTML = "Mesurez votre modèle témoin avec l'application FitSize";
+                descTag.innerHTML = "Juste une photo est nécessaire pour prendre les mesures, la application vous recommandera automatiquement la taille réelle ajustée !";
                 break;
             
             case 250:
-                txtTag.innerHTML = 'Text 2';
+                txtTag.innerHTML = "Faire la retouche avant livraison avec l'application FitSize";
+                descTag.innerHTML = "L'application vous indiquera automatiquement la mesure réelle de vos retouches ajustées au modèle de votre boutique.";
                 break;
 
             case 500:
-                txtTag.innerHTML = 'Text 3';
+                txtTag.innerHTML = "Mesurez votre pied avec l'application FitSize";
+                descTag.innerHTML = "Juste une photo est nécessaire pour prendre les mesures, la application vous recommandera automatiquement la pointure de votre pied !";
                 break;
 
             case 750:
-                txtTag.innerHTML = 'Text 4';
+                txtTag.innerHTML = "Mesurez votre monture avec l'application FitSize";
+                descTag.innerHTML = "Juste une photo est nécessaire pour prendre les mesures, la application vous recommandera automatiquement mesure réelle de vos lunettes !";
                 break;
 
             case 1000:
-                txtTag.innerHTML = 'Text 5';
+                txtTag.innerHTML = "Mesurez votre bague avec l'application FitSize";
+                descTag.innerHTML = "Juste une photo est nécessaire pour prendre les mesures, la application vous recommandera automatiquement mesure réelle de votre bague ajustée !";
                 break;
 
             default:
